2f58aaed91df1cc61d9652d96665547803d4e7f5
Mattaru <67161027+Mattaru@users.noreply.github.com>
2020-12-01 13:18:13 +0200

    The searching forms has been rewriting with django.forms

--- cat_app/catalog/views.py
+++ cat_app/catalog/views.py
@@ -3,39 +3,25 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from catalog.models import Car
+from catalog.forms import SearchForm, OneRowSearch
 
 
 class CarList(ListView):
     model = Car
     template_name = 'car-list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(CarList, self).get_context_data( **kwargs)
+        context['optional_search'] = SearchForm()
+        context['one_row_search'] = OneRowSearch()
+
+        return context
+
     def get_queryset(self):
         q_set = Car.objects.all()
         params = self.request.GET.dict()
 
-        # search for a big form with many filters
-        prod = params.get('producer')
-        mod = params.get('model')
-        year = params.get('release_year')
-        trans = params.get('transmission')
-        color = params.get('color')
-
-        if prod:
-            q_set = q_set.filter(producer__icontains=prod)
-
-        if mod:
-            q_set = q_set.filter(model__icontains=mod)
-
-        if year:
-            q_set = q_set.filter(release_year__icontains=year)
-
-        if trans:
-            q_set = q_set.filter(transmission__icontains=trans)
-
-        if color:
-            q_set = q_set.filter(color__icontains=color)
-
-        # search for simple form
+        # search for the homework
         if params.get('search'):
             val = params.get('search')
             q_set = Car.objects.filter(
@@ -45,5 +31,27 @@ class CarList(ListView):
                 | Q(transmission__icontains=val)
                 | Q(color__icontains=val)
             )
+        else:
+            # search for a big form with many filters
+            prod = params.get('producer')
+            mod = params.get('model')
+            year = params.get('release_year')
+            transmission = params.get('transmission')
+            color = params.get('color')
+
+            if prod:
+                q_set = q_set.filter(producer__icontains=prod)
+
+            if mod:
+                q_set = q_set.filter(model__icontains=mod)
+
+            if year:
+                q_set = q_set.filter(release_year__icontains=year)
+
+            if transmission and not transmission == 'All':
+                q_set = q_set.filter(transmission__icontains=transmission)
+
+            if color:
+                q_set = q_set.filter(color__icontains=color)
 
         return q_set
